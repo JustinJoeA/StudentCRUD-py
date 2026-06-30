0915d06d99691d445d8b8b06a327f8967cb43863
JustinJoeA <zecolder@gmail.com>
2026-06-30 17:51:59 +0700

    Added OOP and JSON files

--- schoolCRUD.py
+++ schoolCRUD.py
@@ -1,92 +1,155 @@
-status = False
-studentsCollection = []
+import json
+
+
+class Student:
+    def __init__(self, name, grade, gpa):
+        self.name = name
+        self.grade = grade
+        self.set_gpa(gpa)
+    
+    def get_gpa(self):
+        return self.__gpa
+    
+    def set_gpa(self, gpa):
+        if 0.00 < gpa < 4.00:
+            self.__gpa = gpa
+        else:
+            raise ValueError('GPA value must be inbetween 0.00 and 4.00!')
+    
+
+
+with open('studentData.json', 'r') as f:
+    studentData = json.load(f)
+    students = []
+    for student in studentData["student"]:
+        studentList = Student(student["name"], student["grade"], student["gpa"])
+        students.append(studentList)
+
+
+
+def studentUpdate():
+    updatedDataStudent = {"student":[]}
+    for student in students:
+        updatedDataStudent["student"].append({"name": student.name, "grade": student.grade, "gpa": student.get_gpa()})
+        
+    with open('studentData.json', 'w') as f:
+        json.dump(updatedDataStudent, f, indent=4)
+        
 
 
 def studentAdd():
     while True:
-        StudentName = input('Please input student`s name: ')
-        
+        studentName = ''
+        while studentName == '':
+            studentName = input('Please input student`s name: ')
+            if studentName == '':
+                print('Student name must be filled!')
+            
         try:
-            StudentScore = float(input('Please input corresponding student`s score: '))
-        except ValueError:
-            print('Please input a float type number! (0.00)')
-        else:
-            studentData = [StudentName, StudentScore]
-            
-            while True:    
-            
-                confirmInput = input(f'Input {studentData} into database? (y/n) ')
-
-                if confirmInput == 'y':
-                    print(f'{StudentName} data has successfully added to the database!')
-                    studentsCollection.append(studentData)
-                        
-                    while True:
-                        continueInput = input('Continue adding student`s data? (y/n) ')
-                        if continueInput == 'n':
-                            return
-                        elif continueInput == 'y':
-                            break
-                        else:
-                            print('Please input a y or n!')  
-                            
-                elif confirmInput == 'n':
-                    while True:
-                        continueInput = input('Inputted data would be deleted, continue? (y/n)')
-                        if continueInput == 'y':
-                            studentData.clear()
-                            print('Inputted data has been deleted! ')
-                            print('Returning to main menu...')
-                            return
-                        elif continueInput == 'n':
-                            break
-                        else:
-                            print('Please input a y or n!')  
-                            
+            while True:
+                studentGrade = int(input('Please input student grade: '))
+                if 9 <= studentGrade <= 12:
+                    try:
+                        studentGpa = float(input('Please input corresponding student`s score: '))
+                    except ValueError:
+                        print('Please input a float type number! (0.00)')
+                    else:
+                        studentData = {"name":studentName,"grade":studentGrade,"gpa":studentGpa}
+                        break
                 else:
-                    print('Please input a y or n!') 
-                
-                break
+                    print('Please input a number between 9 and 12!')
+        except ValueError:
+            print('Please input a number!')     
+            
+        while True:    
+            while True:
+                try:
+                    tempData = Student(studentData["name"], studentData["grade"], studentData["gpa"])
+                    students.append(tempData)
+                    break
+                except ValueError as e:
+                    print(e)
+                    studentGpa = float(input('Please re-enter a valid GPA: '))
+                    studentData["gpa"] = studentGpa
+            
+            confirmInput = input(f'Input {studentData} into database? (y/n) ')
 
+            if confirmInput == 'y':
+                print(f'{studentName} data has successfully added to the database!')
+                studentUpdate()
+                
+                while True:
+                    continueInput = input('Continue adding student`s data? (y/n) ')
+                    if continueInput == 'n':
+                        return
+                    elif continueInput == 'y':
+                        break
+                    else:
+                        print('Please input a y or n!')  
+                        
+            elif confirmInput == 'n':
+                while True:
+                    continueInput = input('Inputted data would be deleted, continue? (y/n)')
+                    if continueInput == 'y':
+                        studentData.clear()
+                        print('Inputted data has been deleted! ')
+                        print('Returning to main menu...')
+                        return
+                    elif continueInput == 'n':
+                        break
+                    else:
+                        print('Please input a y or n!')  
+                        
+            else:
+                print('Please input a y or n!') 
+            
+            
+        
 def studentView():
-    if studentsCollection == []:
+    count = 1
+    if students == []:
         print('Student data is still empty!')
         return
     else:
-        count = 1
-        print(f'{'No.':<5}{'Name':<20} {'Score'}')
-        print('-' * 30)
-        for student in studentsCollection:
-            print(f'{count:<5} {student[0]:<20} {student[1]}')
-            count+=1
+        print(f'{'No.':<5}{'Name':<20} {'Grade':<10} {'Score:'}')
+        print('-' * 45)
+        for student in students:
+            print(f'{count:<5} {student.name:<20} {student.grade:<10} {student.get_gpa()}')
+            count += 1
         return
 
 
+
 def studentDelete():
-    if studentsCollection == []:
+    if students == []:
         print('Student data is still empty!')
         return
     else:
         while True:
             count = 1
-            print(f'{'No.':<5}{'Name':<20} {'Score'}')
-            print('-' * 30)
-            for student in studentsCollection:
-                print(f'{count:<5} {student[0]:<20} {student[1]}')
-                count+=1
-
-            inputDelete = int(input('Please select the student that want to be deleted... (0 to exit)'))
+            print(f'{'No.':<5}{'Name':<20} {'Grade':<10} {'Score:'}')
+            print('-' * 45)
+            for student in students:
+                print(f'{count:<5} {student.name:<20} {student.grade:<10} {student.get_gpa()}')
+                count += 1
             
-            if inputDelete == 0:
-                return
-            elif type(inputDelete) is int:
-                inputDelete = inputDelete - 1
-                print(f'{studentsCollection[inputDelete][0]}`s data has been deleted!\n')
-                studentsCollection.pop(inputDelete)
-                return
-            else:
-                print('Please enter a valid parameter!')
-    
+            try:    
+                inputDelete = int(input('Please select the student that want to be deleted... (0 to exit)'))
+                if inputDelete == 0:
+                    return
+                elif type(inputDelete) is int:
+                    inputDelete = inputDelete - 1
+                    print(f'{students[inputDelete].name}`s data has been deleted!\n')
+                    students.pop(inputDelete)
+                    studentUpdate()
+                    return
+                else:
+                    print('Please enter a valid parameter!')
+            except ValueError:
+                print('Please input a number!')
+            except IndexError:
+                print('Please input available column only!')
+            
 
 def mainMenu():
     while True:
@@ -113,19 +176,6 @@ def mainMenu():
                 break
             else:
                 print('Please select a valid menu!')
-        
 
-while status == False:
-    try:
-        userLogin=input('Welcome user, do you want to login? (y/n) ')
-    except:
-        print('Invalid input, please try again!')
-    else:
-        if userLogin == 'y':
-            status = True
-            mainMenu()
-        elif userLogin == 'n':
-            print('Program terminated, have a nice day!')
-            break
-        else:
-            print('Invalid input, please try again!')
+
+mainMenu()
